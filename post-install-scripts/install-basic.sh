ec5f5167694ea7cd26f73d04f219cf5f7f28bd8c
Abled-Taha <abledtaha@gmail.com>
2026-04-24 01:10:05 +0500

    Regular Commit

--- post-install-scripts/install-basic.sh
+++ post-install-scripts/install-basic.sh
@@ -1,5 +1,5 @@
 #!
 
-yay -S --needed --noconfirm stow mise zen-browser-bin vlc vlc-plugins-all gimp audacity kdenlive obs-studio visual-studio-code-bin steam kitty gnome-keyring libsecret nwg-look arc-gtk-theme papirus-icon-theme qt5ct qt6ct qt5-wayland qt6-wayland kvantum hyprland-qt-support udisks2 udiskie ntfs-3g polkit-gnome rofi pavucontrol nautilus qbittorrent
+yay -S --needed --noconfirm stow mise zen-browser-bin vlc vlc-plugins-all gimp audacity kdenlive obs-studio visual-studio-code-bin steam kitty gnome-keyring libsecret nwg-look arc-gtk-theme papirus-icon-theme qt5ct qt6ct qt5-wayland qt6-wayland kvantum hyprland-qt-support udisks2 udiskie ntfs-3g polkit-gnome rofi ttf-jetbrains-mono-nerd pavucontrol nautilus qbittorrent
 
 flatpak install flathub com.heroicgameslauncher.hgl com.usebottles.bottles
