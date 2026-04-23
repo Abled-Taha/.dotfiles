9fa858195910e8a73b3c9678a81538a5fd66812f
Abled-Taha <abledtaha@gmail.com>
2026-04-24 01:04:45 +0500

    Regular Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+WIP
+
+Quite Messy, don't want to write docs, and will probably hate myself when I have to use this, but it is what it is.
---
+++ main.sh
@@ -0,0 +1,3 @@
+#!
+
+# Some main function to run every stow command but also giving user the choice of what to stow? I don't fucking know anything, I WANNA SLEEEEP!!!
