44dfa3f8c9cc29b9ad74c0ef27faf30892c5d23d
Abled-Taha <abledtaha@gmail.com>
2026-05-30 21:00:44 +0500

    Regular Commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,63 @@
+if status is-interactive
+    # Initialize Starship Prompt 
+    starship init fish | source
+
+    # Initialize Mise Environment Manager 
+    if type -q mise
+        mise activate fish | source
+    end
+end
+
+# Disable the default greeting message 
+set -g fish_greeting ""
+
+# Set environment variables securely 
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+# Ported Environment Variables & Quick Navigation Paths 
+set -gx oracle "abled.minecraftr.us"
+set -gx STORAGE /mnt/Storage
+set -gx projects "$STORAGE/Documents/Projects"
+set -gx notes "$STORAGE/Documents/Obsidian"
+set -gx bashrc "$HOME/.bashrc"
+set -gx dotfiles "$HOME/.dotfiles"
+set -gx hyprland "$dotfiles/hypr/.config/hypr/hyprland.conf"
+set -gx minecraft "$HOME/.local/share/PrismLauncher/instances"
+
+# Modern File Listing & Utilities Aliases 
+alias ls="eza --icons --color=always --group-directories-first"
+alias ll="eza -lah --icons --color=always --group-directories-first"
+alias la="eza -a --icons"
+alias tree="eza --tree --icons"
+alias cat="bat --style=plain"
+alias preview="bat"
+alias grep="grep --color=auto"
+alias g="git"
+
+# Shell Management 
+alias reload="source ~/.config/fish/config.fish"
+
+# VM Management (Libvirt / Virsh) 
+## Status Check 
+alias vm-status="sudo virsh list --all"
+
+## VM Reset 
+alias reset-archlinux="sudo virsh snapshot-revert archlinux --snapshotname base"
+alias reset-debian13="sudo virsh snapshot-revert debian13 --snapshotname base"
+alias reset-win10="sudo virsh snapshot-revert win10 --snapshotname base"
+
+## VM Power On 
+alias start-archlinux="sudo virsh start archlinux"
+alias start-debian13="sudo virsh start debian13"
+alias start-win10="sudo virsh start win10"
+
+## Clean ACPI Shutdown (Graceful) 
+alias stop-archlinux="sudo virsh shutdown archlinux"
+alias stop-debian13="sudo virsh shutdown debian13"
+alias stop-win10="sudo virsh shutdown win10"
+
+## Forced Stop (Instant Kill / Hard Reset power-off) 
+alias kill-archlinux="sudo virsh destroy archlinux"
+alias kill-debian13="sudo virsh destroy debian13"
+alias kill-win10="sudo virsh destroy win10"
--- post-install-scripts/install-basic.sh
+++ post-install-scripts/install-basic.sh
@@ -1,5 +1,5 @@
 #!
 
-yay -S --needed --noconfirm stow mise zen-browser-bin vlc vlc-plugins-all gimp audacity kdenlive obs-studio visual-studio-code-bin steam kitty gnome-keyring libsecret nwg-look arc-gtk-theme papirus-icon-theme qt5ct qt6ct qt5-wayland qt6-wayland kvantum udisks2 udiskie ntfs-3g polkit-gnome wmenu ttf-jetbrains-mono-nerd pavucontrol nautilus qbittorrent neovim npm ripgrep wl-clipboard fd mangowm swaybg dunst slurp
+yay -S --needed --noconfirm stow mise zen-browser-bin vlc vlc-plugins-all gimp audacity kdenlive obs-studio visual-studio-code-bin steam kitty gnome-keyring libsecret nwg-look arc-gtk-theme papirus-icon-theme qt5ct qt6ct qt5-wayland qt6-wayland kvantum udisks2 udiskie ntfs-3g polkit-gnome wmenu ttf-jetbrains-mono-nerd pavucontrol nautilus qbittorrent neovim npm ripgrep wl-clipboard fd mangowm swaybg dunst slurp fish brightnessctl starship eza bat
 
 flatpak install flathub com.heroicgameslauncher.hgl com.usebottles.bottles
